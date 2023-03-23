572fbc5ae811a794774756237bc1e8d904a29f67
spidernak <127380223+spidernak@users.noreply.github.com>
2023-03-23 23:05:39 +0700

    Update Ass.java

--- Tp/Ass.java
+++ Tp/Ass.java
@@ -1,7 +1,16 @@
 package Tp;
+
+import java.util.Scanner;
+
 public class Ass{
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Hello kon papa");
+        int a;
+        System.out.print("Please input a: ");
+        a = input.nextInt();
+        a = a*20;
+        System.out.println("Result"+a);
     }
 }
     
